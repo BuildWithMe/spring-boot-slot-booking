2aef09da33ef8e3d99b43a03a7d29ddcea4a376f
shahbaz03 <shahbazalam03@gmail.com>
2015-08-22 22:04:05 +0530

    initial commit

---
+++ SlotBooking/src/main/java/com/services/SlotBooking/App.java
@@ -0,0 +1,13 @@
+package com.services.SlotBooking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
